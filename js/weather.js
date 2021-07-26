baa3cab1c9f4d402f7605d1acdd490705e071f97
hanbinchoi <wkghskak@naver.com>
2021-07-26 19:10:32 +0900

    ..

--- js/weather.js
+++ js/weather.js
@@ -5,7 +5,7 @@ const API_KEY = "0dbd18c87c76b1c2769e97177bf57131";
 function onGeoOk(position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
     fetch(url)
     .then((response) => response.json())
     .then((data) => {
