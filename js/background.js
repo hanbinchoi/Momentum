5553fd6033b7868c998438f7e5c8d52eabf53398
hanbinchoi <wkghskak@naver.com>
2021-07-26 19:04:02 +0900

    ..

--- js/background.js
+++ js/background.js
@@ -12,4 +12,4 @@ const images = [
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const body = document.body;
-body.style.background = `linear-gradient( rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4) ),url("img/${chosenImage}`;
+body.style.background = `linear-gradient( rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4) ),url("momentum/img/${chosenImage}`;
